e36e3b2f381655bbc17e0b90a2154e78e42b807e
Guochun.Gong <yqsy021@126.com>
2017-06-30 07:34:39 +0800

    add 05_replace_spaces

---
+++ 05_replace_spaces/replace_spaces.cpp
@@ -0,0 +1,16 @@
+// 题目:替换空格
+// 时间:O(n),空间:O(1)
+
+#include <algorithm>
+#include <stdlib.h>
+#include <iostream>
+
+
+#include "gtest/gtest.h"
+
+
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::FLAGS_gtest_death_test_style = "fast";
+    return RUN_ALL_TESTS();
+}
